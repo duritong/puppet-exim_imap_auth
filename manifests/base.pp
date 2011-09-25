0ba32561c92811f6b925a3dd7ad548a232268260
o <o@immerda.ch>
2011-09-25 15:20:05 +0200

    ensure package

---
+++ manifests/base.pp
@@ -0,0 +1,5 @@
+class exim-imap-auth::base {
+    package { 'exim-imap-auth':
+        ensure => present,
+    }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,5 @@
 # manifests/init.pp - module to manage exim imap auth
+
+class exim-imap-auth {
+    include exim-imap-auth::base
+}
