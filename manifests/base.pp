efe054c4ec2cc791dd20283b05f0b6840f8241c6
o <o@immerda.ch>
2011-10-28 13:50:34 +0200

    remove the package in the config

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,8 +1,4 @@
 class exim-imap-auth::base {
-    package { 'exim-imap-auth':
-        ensure => absent,
-    }
-
     file{'/etc/exim/imap-auth.pl':
       source => "puppet:///modules/exim-imap-auth/imap-auth.pl",
       require => Package['exim'],
