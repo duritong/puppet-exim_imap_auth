f5fe9d1ae19280809a9fb39d4ad54c7c0e99f53e
mh <mh@immerda.ch>
2014-02-08 00:46:31 +0100

    add missing dependency

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,12 +1,16 @@
+# install everything needed for the imap auth
 class exim_imap_auth::base {
-  package { 'perl-Net-IMAP-Simple-SSL':
+  package { [ 'perl-Net-IMAP-Simple-SSL',
+              'perl-Net-IMAP-Simple', ]:
     ensure => present,
   }
 
   file{'/etc/exim/imap-auth.pl':
-    source => "puppet:///modules/exim_imap_auth/imap-auth.pl",
+    source  => 'puppet:///modules/exim_imap_auth/imap-auth.pl',
     require => Package['exim'],
-    before => Service['exim'],
-    owner => root, group => root, mode => 0755;
+    before  => Service['exim'],
+    owner   => root,
+    group   => 0,
+    mode    => '0755';
   }
 }
